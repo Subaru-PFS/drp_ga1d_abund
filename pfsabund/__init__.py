5d68ba9dfa2baab35e8f3b13939c8398a9ccfd5a
iaescala <46059540+iaescala@users.noreply.github.com>
2020-08-03 17:19:12 -0700

    changed import structure

--- pfsabund/__init__.py
+++ pfsabund/__init__.py
@@ -2,4 +2,6 @@ from __future__ import absolute_import
 
 from . import pfs_io as io
 from . import pfs_read_synth as read
+from . import smooth_gauss
+from . import pfs_phot as phot
 from . import pfs_utilities as ut
