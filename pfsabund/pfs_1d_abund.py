c8a5f72bc65ae35a3bc6e77e32506b4a3c95d15c
enkirby <enk@astro.caltech.edu>
2023-06-23 18:24:08 -0400

    Do not hard-code Teff and logg.

--- pfsabund/pfs_1d_abund.py
+++ pfsabund/pfs_1d_abund.py
@@ -54,8 +54,8 @@ from pfsabund import pfs_distance as dist
 # MNI -- END --
 
 # ENK -- BEGIN --
-from lsfconv.psf import *
-from lsfconv.resampling.fluxconservingresampler import FluxConservingResampler
+#from lsfconv.psf import *
+#from lsfconv.resampling.fluxconservingresampler import FluxConservingResampler
 # ENK -- END --
 
 
@@ -156,16 +156,16 @@ class MeasurePFSAbund():
         #Store initial guess values in new variables
         self.feh0 = self.feh_def
         self.alphafe0 = self.alphafe_def
-        #self.teff0 = pfs.prop('teffphot')
-        self.teff0 = 4210.   #ENK: changed Teff to match "true" value
-        #self.tefferr0 = pfs.prop('teffphoterr')   #ENK: new property tefferr0
-        self.tefferr0 = 0.0001                          #ENK: arbitrary value
+        self.teff0 = pfs.prop('teffphot')
+        #self.teff0 = 4210.   #ENK: changed Teff to match "true" value
+        self.tefferr0 = pfs.prop('teffphoterr')   #ENK: new property tefferr0
+        #self.tefferr0 = 0.0001                          #ENK: arbitrary value
         
         #If fitting for the surface gravity, set logg to the default value
         #otherwise, fix to the photometric value
         if fit_logg: self.logg0 = self.logg_def
-        #else: self.logg = pfs.prop('loggphot')
-        else: self.logg = 1.3   #ENK: changed Teff to match "true" value
+        else: self.logg = pfs.prop('loggphot')
+        #else: self.logg = 1.3   #ENK: changed Teff to match "true" value
         
         #Execute abundance measurement
         self.measure_abund(pfs, fit_logg=fit_logg)
@@ -205,8 +205,8 @@ class MeasurePFSAbund():
         pfs.assign(pfs.prop('initcont'), 'refinedcont')
 
         #ENK -- BEGIN --
-        gauss = GaussPsf(pfs.prop('wvl'), sigma=self.dlam)
-        self.pca = PcaPsf.from_psf(gauss, pfs.prop('wvl'))
+        #gauss = GaussPsf(pfs.prop('wvl'), sigma=self.dlam)
+        #self.pca = PcaPsf.from_psf(gauss, pfs.prop('wvl'))
         #ENK -- END --
         
         while i < self.maxiter:
